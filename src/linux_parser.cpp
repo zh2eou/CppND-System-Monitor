bad004d63003a556df06a2670cd2e272a2bff953
zh2eou <zh2eou@gmail.com>
2020-04-17 20:39:24 -0400

    remove commented out filesystem implementation now that it's in a separate branch

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -9,10 +9,6 @@ using std::string;
 using std::to_string;
 using std::vector;
 
-// #include <filesystem>
-// using std::filesystem::directory_iterator;
-// using std::filesystem::is_directory;
-
 // DONE: An example of how to read data from the filesystem
 string LinuxParser::OperatingSystem() {
   string line;
@@ -49,6 +45,7 @@ string LinuxParser::Kernel() {
   return kernel;
 }
 
+// Updated to use std::filesystem on filesystem branch
 vector<int> LinuxParser::Pids() {
   vector<int> pids;
   DIR* directory = opendir(kProcDirectory.c_str());
@@ -67,23 +64,6 @@ vector<int> LinuxParser::Pids() {
   closedir(directory);
   return pids;
 }
-
-// Updated to use std::filesystem
-// vector<int> LinuxParser::Pids() {
-//   vector<int> pids;
-  
-//   for (const auto& file : directory_iterator(kProcDirectory)) {
-//     if (is_directory(file.status())) {
-//       string filename = file.path().filename();
-//       if (std::all_of(filename.begin(), filename.end(), isdigit)) {
-//         int pid = stoi(filename);
-//         pids.push_back(pid);
-//       }
-//     }
-//   }
-  
-//   return pids;
-// }
 
 // Read and return the system memory utilization
 float LinuxParser::MemoryUtilization() {
