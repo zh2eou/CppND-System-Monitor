f3849afeeff20bfb75762414c641e02f4a6482ac
zh2eou <zh2eou@gmail.com>
2020-04-17 22:00:37 -0400

    Remove constructor and just initialize private members to 0

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,11 +1,6 @@
 #include "processor.h"
 #include "linux_parser.h"
 
-Processor::Processor() {
-   prevTotal_ = LinuxParser::Jiffies();
-   prevActive_ = LinuxParser::ActiveJiffies();
-}
-
 // Return the aggregate CPU utilization
 float Processor::Utilization() { 
    long totalJiffies = LinuxParser::Jiffies();
